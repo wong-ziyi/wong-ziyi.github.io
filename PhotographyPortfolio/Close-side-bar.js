fbb2e4acb51f3cf2dafecc563a3698d5317d3f3d
elsinore <elsinore@users.noreply.github.com>
2018-04-25 08:50:28 +0900

    20180425
    20180425

---
+++ PhotographyPortfolio/Close-side-bar.js
@@ -0,0 +1,4 @@
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
---
+++ PhotographyPortfolio/Open-side-bar.js
@@ -0,0 +1,4 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
